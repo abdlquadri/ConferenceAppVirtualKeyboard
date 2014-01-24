effe1242a5c10189d490e28c5885d2c47c9cd948
abdlquadri <abdlquadri@googlemail.com>
2014-01-24 04:09:00 +0000

    RESOLVED: refactored package names Conflicts: 	src/com/javafx/experiments/scheduleapp/control/VirtualKeyboardSkin.java 	src/demo/VKDemo.java

--- src/com/javafx/experiments/scheduleapp/control/VirtualKeyboardSkin.java
+++ src/com/javafx/experiments/scheduleapp/control/VirtualKeyboardSkin.java
@@ -31,23 +31,19 @@
  */
 package com.javafx.experiments.scheduleapp.control;
 
-import com.sun.javafx.robot.FXRobot;
 import com.sun.javafx.scene.control.behavior.BehaviorBase;
 import com.sun.javafx.scene.control.skin.SkinBase;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
-import javafx.scene.control.TextInputControl;
 //import javafx.scene.control.SkinBase;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
